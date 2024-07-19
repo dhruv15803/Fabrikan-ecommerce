2155cca8ff7ef990614e51a186799d7db04adc9e
dhruv15803 <Dhruvshetty960@gmail.com>
2024-07-19 17:30:32 +0530

    confirmation alert for product delete

--- frontend/src/Pages/AdminProducts.tsx
+++ frontend/src/Pages/AdminProducts.tsx
@@ -8,22 +8,28 @@ import axios from "axios";
 import { backendUrl } from "../App";
 
 const AdminProducts = () => {
-  const {isLoading:isProductsLoading,products,setProducts} = useGetProducts();
+  const {
+    isLoading: isProductsLoading,
+    products,
+    setProducts,
+  } = useGetProducts();
   const [isAddProduct, setIsAddProduct] = useState<boolean>(false);
 
-  const removeProduct = async (productId:string) => {
+  const removeProduct = async (productId: string) => {
     const oldProducts = products;
     try {
-      const newProducts = products.filter((product) => product._id!==productId);
+      const newProducts = products.filter(
+        (product) => product._id !== productId
+      );
       setProducts(newProducts);
-      await axios.delete(`${backendUrl}/api/product/${productId}`,{
-        withCredentials:true,
+      await axios.delete(`${backendUrl}/api/product/${productId}`, {
+        withCredentials: true,
       });
     } catch (error) {
       console.log(error);
       setProducts(oldProducts);
     }
-  }
+  };
 
   return (
     <>
@@ -35,17 +41,27 @@ const AdminProducts = () => {
         >
           {!isAddProduct ? "Add product" : "Cancel"}
         </Button>
-        {isAddProduct && <AdminAddProductForm setProducts={setProducts}/>}
+        {isAddProduct && <AdminAddProductForm setProducts={setProducts} />}
       </div>
-      {isProductsLoading && <div className="flex justify-center my-4 items-center">
-          <Loader height="80" width="80" color="black"/>
+      {isProductsLoading && (
+        <div className="flex justify-center my-4 items-center">
+          <Loader height="80" width="80" color="black" />
           <span className="text-lg font-semibold">Loading products...</span>
-        </div>}
-      {!isProductsLoading && products.length!==0 && <div className="flex flex-wrap mx-10 my-4 gap-2 p-2">
-        {products.map((product) => {
-          return <AdminProductCard key={product._id} product={product} removeProduct={removeProduct}/>
-        })}
-      </div>}
+        </div>
+      )}
+      {!isProductsLoading && products.length !== 0 && (
+        <div className="flex flex-wrap mx-10 my-4 gap-2 p-2">
+          {products.map((product) => {
+            return (
+              <AdminProductCard
+                key={product._id}
+                product={product}
+                removeProduct={removeProduct}
+              />
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
--- frontend/src/components/AdminProductCard.tsx
+++ frontend/src/components/AdminProductCard.tsx
@@ -1,14 +1,25 @@
 import { Product } from "../types";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
   product: Product;
-  removeProduct:(id:string) => Promise<void>;
+  removeProduct: (id: string) => Promise<void>;
 };
 
-const AdminProductCard = ({ product,removeProduct}: Props) => {
-    const navigate = useNavigate();
+const AdminProductCard = ({ product, removeProduct }: Props) => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex flex-col p-2 border rounded-lg shadow-md gap-2">
@@ -21,8 +32,32 @@ const AdminProductCard = ({ product,removeProduct}: Props) => {
           <span className="font-semibold">₹</span>
           <span>{product.productPrice}</span>
         </div>
-        <Button onClick={() => navigate(`/admin/product/${product._id}`) } className="bg-gray-200" variant="outline">Update</Button>
-        <Button onClick={() => removeProduct(product._id)} variant="destructive">Remove</Button>
+        <Button
+          onClick={() => navigate(`/admin/product/${product._id}`)}
+          className="bg-gray-200"
+          variant="outline"
+        >
+          Update
+        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive">Remove</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to remove this product?
+              </AlertDialogTitle>
+              <AlertDialogDescription></AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={() => removeProduct(product._id)}>
+                Confirm
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </>
   );
